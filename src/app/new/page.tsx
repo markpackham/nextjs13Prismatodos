7f5db2fdc8288ab70107d9adb84efaeb96252509
mark <markpackham1@gmail.com>
2023-05-24 12:43:40 +0100

    admin new section
    https://youtu.be/NgayZAuTgwM?t=1078

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
     <>
-      <h1>New Page (/new)</h1>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
     </>
   );
 }
